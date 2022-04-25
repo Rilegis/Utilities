b93e36fd1fcc28b7bd31acc2b6ee16165d443eae
Rilegis <13032631+Rilegis@users.noreply.github.com>
2022-04-25 03:01:10 +0200

    Added rsync service

--- Boilerplates/Docker/minecraft-stack/.backups/do_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# W.I.P
-# Add backup loop
-#   - Execute every 30min
-#   - Compact backup into tarball
-#   - Keep last 15 days of backups---maybe 7?
---
+++ Boilerplates/Docker/minecraft-stack/.server-backups/do_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# DISCLAIMER: THIS IS A WORK IN PROGRESS SCRIPT, IT IS NOT MEANT TO BE USED IN PRODUCTION, YET.
+
+########## START USER-EDIT SECTION ##########
+SOURCE_LOCATION=/usr/data/mc_server                     ## Source directory (IMPORTANT: Path without trailing '/')
+BACKUP_LOCATION=/usr/data/backups                       ## Backup directory (IMPORTANT: Path without trailing '/')
+BACKUP_RETAIN_DAYS=15                                   ## Number of days to keep local tarball archives
+########## STOP USER-EDIT SECTION ###########
+
+####### DO NOT EDIT AFTER THIS POINT ########
+while true
+do
+  # Sleep for 30 minutes......why am i even commenting this?
+  echo "Waiting 30m for next backup execution..."
+  sleep 30m
+  echo "Backup in progress..."
+
+  # Create a filename suffix
+  FILENAME_SUFFIX=`date +%F-%H-%M`
+
+  # Execute server backup
+  mkdir -p $BACKUP_LOCATION/mc_server # Make sure the backup location exists
+  rsync -a --delete $SOURCE_LOCATION/ $BACKUP_LOCATION/mc_server # Generates a synced backup
+
+  # Archive synced backup into tarball for redundancy
+  tar -czf mc_server_$FILENAME_SUFFIX.tar.gz $BACKUP_LOCATION
+
+  # Delete tarballs archives that are older than 'BACKUP_RETAIN_DAYS' days
+  find $BACKUP_LOCATION -type f  -name "mc_server_*.tar.gz" -mtime +$BACKUP_RETAIN_DAYS -exec rm {} \;
+
+  echo "Backup completed."
+done
