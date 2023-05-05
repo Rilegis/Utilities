d37f8b9ccc8371cfa0af6d683a5b24926095a5ac
Rilegis <13032631+Rilegis@users.noreply.github.com>
2023-05-05 22:16:18 +0200

    mod: added docker volume linking for cups-pdf

--- Boilerplates/Docker/cups/build/docker-entrypoint.sh
+++ Boilerplates/Docker/cups/build/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+# Cups PDF docker volume setup
+chmod 777 /var/spool/cups-pdf/ANONYMOUS
+chown root:lpadmin /var/spool/cups-pdf/ANONYMOUS
+
+# Set user "admin" passsword
 echo -e "${ADMIN_PASSWORD}\n${ADMIN_PASSWORD}" | passwd admin
 
+# Do stuff
 if [ ! -f /etc/cups/cupsd.conf ]; then
   cp -rpn /etc/cups-skel/* /etc/cups/
 fi
